aae3fea59ef40c0634a4279f20374d103e805a79
Subroto112001 <subrotokumarbarman@gmail.com>
2025-10-31 15:57:07 +0600

    upgrade

--- src/midleware/authorize.middleware.js
+++ src/midleware/authorize.middleware.js
@@ -3,9 +3,23 @@ const { customError } = require("../utils/customError");
 const authorize = (resource, action) => {
   return async (req, res, next) => {
     try {
-      console.log(req.user.permission);
+      
+      await req.user.populate("permission.permissionId");
+
+      const hasPermission = req.user.permission.find((p) => {
+        return p.permissionId?.name === resource && p.actions.includes(action);
+      });
+
+      if (!hasPermission) {
+        throw new customError(
+          403,
+          "You are not allowed to access this resource"
+        );
+      }
+
+      next(); // ✅ Pass forward if allowed
     } catch (error) {
-      throw new customError(403, "You are not allowed to access this resource");
+      next(new customError(403, "You are not allowed to access this resource"));
     }
   };
 };
